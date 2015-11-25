4280b2dba8cfbf062809610e83990bf5635bbd49
Ryan Melzer <rdmelzer@email.arizona.edu>
2015-11-25 02:33:21 -0700

    pull this shit

---
+++ jarrydIsAFaggot.py
@@ -0,0 +1,12 @@
+def main():
+    while True:
+        c = raw_input("Is Jarryd a faggot? (Y/n) -> ")
+        if c == "Y":
+            print "Yes, yes he is a faggot, a huge faggot"
+        elif c == "n":
+            print "Clearly, you're wrong, and he is a faggot"
+        else:
+            print "enter Y/n you fucking dunce cap"
+
+if __name__ == "__main__":
+    main()
