5a81e10e49c012a619c58c44873cc912665ec226
Jarryd Goodman <jarryd999@gmail.com>
2015-12-08 13:20:53 -0700

    Added remove, update, insert functions to java class.

--- DatabaseController.java
+++ DatabaseController.java
@@ -99,25 +99,25 @@ public class DatabaseController {
   }
 
 
-  public Vector<String> FindAllEmployees() {
-    String sql_query = "SELECT * FROM mccann.employee";
-    try {
-      ResultSet rs  = statement_.executeQuery(sql_query);
-      Vector<String> result_employees = new Vector<String>();
-      while (rs.next()) {
-         String temp_record = rs.getString("fname") + "##" + rs.getString("minit") +
-             "##" + rs.getString("lname") + "##" + rs.getString("ssn") + "##" +
-             rs.getString("bdate") + "##" + rs.getString("address") + "##" +
-	     rs.getString("sex") + "##" + rs.getString("salary") +
-	     "##" + rs.getString("superssn") + "##" + rs.getString("dno");
-        result_employees.add(temp_record);
-      }
-      return result_employees;
-    } catch (SQLException sqlex) {
-      sqlex.printStackTrace();
-    }
-    return null;
-  }
+  // public Vector<String> FindAllEmployees() {
+  //   String sql_query = "SELECT * FROM mccann.employee";
+  //   try {
+  //     ResultSet rs  = statement_.executeQuery(sql_query);
+  //     Vector<String> result_employees = new Vector<String>();
+  //     while (rs.next()) {
+  //        String temp_record = rs.getString("fname") + "##" + rs.getString("minit") +
+  //            "##" + rs.getString("lname") + "##" + rs.getString("ssn") + "##" +
+  //            rs.getString("bdate") + "##" + rs.getString("address") + "##" +
+	 //     rs.getString("sex") + "##" + rs.getString("salary") +
+	 //     "##" + rs.getString("superssn") + "##" + rs.getString("dno");
+  //       result_employees.add(temp_record);
+  //     }
+  //     return result_employees;
+  //   } catch (SQLException sqlex) {
+  //     sqlex.printStackTrace();
+  //   }
+  //   return null;
+  // }
 
   public ArrayList<String[]> DisplayTable(String tableName)
   {
@@ -165,7 +165,7 @@ public class DatabaseController {
 
     try
     {
-      rs = statement.executeQuery(sql_query);
+      rs = statement_.executeQuery(sql_query);
       result = new ArrayList<String[]>();
     }
     catch (SQLException ex)
@@ -187,7 +187,7 @@ public class DatabaseController {
           temp[3] = rs.getString("sex");
           temp[4] = rs.getString("jobTitle");
           temp[5] = rs.getInt("officeID") + "";
-          temp[6] = rs.getInt("phone") + "";
+          temp[6] = rs.getString("phone") + "";
           temp[7] = rs.getString("address");
           temp[8] = rs.getInt("carID") + "";
           break;
@@ -237,5 +237,154 @@ public class DatabaseController {
     } // end of rs.next()
 
     return result;
+  }
+
+  public boolean InsertEmployee(String employeeID, String name, String DOB, String sex, String jobTitle, String officeID, String phone, String address, String cardID)
+  {
+
+  }
+
+
+  /**
+  * Function: Insert(tableName, attrValues)
+  * Description:  Function will insert a row from the given table.  Function assumes
+  *               valid name input for tableName and attrValues, and that the table exists.
+  *               Function also assumes that attribute values are supplied in an order
+  *               corresponding to the order of the attributes in the actual table.
+  *               After the function is run, the specified row should be inserted into the relation.
+  * @input:
+  *         tableName (String) - the name of the table to insert the row into
+  *         attrValues (String[]) - the values of the attributes to insert
+  *
+  * @return: Return True if successful, False otherwise
+  **/
+  public boolean Insert(String tableName, String[] attrValues){
+    StringBuffer query = new StringBuffer();
+    query.append("INSERT INTO ");
+    query.append(tableName);
+    query.append(" (");
+
+    //loop through the array of attributes and add them to the query string
+    for (int i = 0; i < attrValues.len; i++){
+      query.append("'");
+      query.append(attrValues[i]);
+      query.append("'");
+
+      //if not at the last attribute, add a comma seperator
+      if (i+1 < attrValues.len){
+        query.append(",");
+      }
+
+      query.append(")");
+
+      //try to execute the query and return true on success
+      //print stack trace and return false if unsuccessful
+      try{
+        ResultSet rs = statement_.executeQuery(query.toString());
+        return true;
+      } catch (SQLException sqlex){
+        sqlex.printStackTrace;
+        return false;
+      }
+      //execute the
+    }
+  }
+
+  /**
+  * Function: Remove(tableName, attrValues)
+  * Description:  Function will remove a row from the given table.  Function assumes
+  *               valid name input as tableName and attrValues, and that the table exists.
+  *               After the function is run, the specified row should be removed from the relation.
+  * @input:
+  *         tableName (String) - the name of the table to remove the row from
+  *         attrValues (String[][]) - the name of the attribute and the corresponding value for the given row to remove
+  *
+  * @return: Return True if successful, False otherwise
+  **/
+  public boolean Remove(String tableName, String[][] attrValues){
+    StringBuffer query = new StringBuffer();
+    query.append("DELETE FROM ");
+    query.append(tableName);
+    query.append(" WHERE ");
+
+    //loop through the attributes and append them to the WHERE clause
+    for (int i = 0; i < attrValues.len; i++){
+      query.append(attrValues[i]);
+      query.append(" = '");
+      query.append(attrValues[i][0]);
+      query.append("' ");
+
+      //if not at the last attribue, add an AND clause
+      if (i+1 < attrValues.len){
+        query.append(" AND ");
+      }
+
+      //try to execute the query and return true on success
+      //print stack trace and return false if unsuccessful
+      try{
+        statement_.executeQuery(query.toString());
+        return true;
+      } catch (SQLException sqlex){
+        sqlex.printStackTrace();
+        return false;
+      }
+    }
+  }
+
+  /**
+  * Function: Update(tableName, attrValues, oldPrimaryKeys)
+  * Description:  Function will modify a row from the given table.  Function assumes
+  *               valid name input as tableName and attrValues, and that the table exists.
+  *               After the function is run, the specified row should be modified to reflect specified data.
+  * @input:
+  *         tableName (String) - the name of the table to remove the row from
+  *         attrValues (String[][]) - the name of the attribute and the corresponding value to update row with
+  *         oldPrimaryKeys (String[][]) - the name of the PK's with old values to identify row to update
+  *
+  * @return: Return True if successful, False otherwise
+  **/
+  public boolean Update(String tableName, String[][] attrValues, String[][] oldPrimaryKeys){
+        StringBuffer query = new StringBuffer();
+    query.append("UPDATE ");
+    query.append(tableName);
+    query.append(" WHERE ");
+
+    //loop through and identify the row by its old data
+    for (int i = 0; i < oldPrimaryKeys.len; i++){
+      query.append(oldPrimaryKeys[i])
+      query.append(" = '");
+      query.append(oldPrimaryKeys[i][0]);
+
+      //if not at the last attribute, add an AND clause
+      if (i+1 < oldPrimaryKeys.len){
+        query.append(" AND ");
+      }
+    }
+
+    query.append(" SET ");
+
+    //loop through the new attrValues and set them
+    for (int i = 0; i < attrValues.len; i++){
+      query.append(attrValues[i]);
+      query.append(" = ");
+      query.append(attrValues[i][0]);
+
+      //if not on the last iteration of the loop, add a comma seperator
+      if (i+1 < attrValues.len){
+        query.append(", ");
+      }
+    }
+
+    //try to execute the query and return true on success
+    //print stack trace and return false if unsuccessful
+    try{
+      statement_.executeQuery(query.toString());
+      return true;
+    } catch (SQLException sqlex){
+      sqlex.printStackTrace();
+      return false;
+    }
+
+
   }
 }
